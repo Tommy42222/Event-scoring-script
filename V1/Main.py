5c3a349b048fdb9ff77d983741d6c56416b26946
Tom Horsman <thomashorsman99@gmail.com>
2025-04-17 16:51:36 +0100

    add solo player input, and player event select

--- V1/Main.py
+++ V1/Main.py
@@ -11,45 +11,99 @@ playerScoreDict = {}
 teamEventList = [[],[]]
 
 # holds the academic and sporty SOLO events
-playerEventList = [[],[]]
+playerEventList = [["Spelling bee","Treasure hunt"],["Sack race", "Running", "Egg and Spoon race"]]
 
 # tracks the SOLO players taking part in each event
 eventPlayersList=[[],[],[],[],[]]
 
 
 print("TEAM INTRO")
+# NOTE COMMENT CODE BACK IN LATER
+# # whle loop for adding each team and asigning their players
+# while True:
 
-# whle loop for adding each team and asigning their players
+#     # Loops for each team
+#     for teamNumber in range(1,5):
+#         print(f"ADDING PLAYERS TO TEAM {teamNumber}")
+        
+#         # Loops for each player in team
+#         for playerNumber in range(1,6):
+
+#             # loops untill valid player is added to team
+#             while True:
+
+#                 userInput = input(f"Enter the name for player {playerNumber} >>> ")
+                
+#                 # validates input:
+#                 # NOTE Add more and propper validation later
+
+#                 if userInput in teamPlayerList[int(teamNumber)-1]:
+#                     print("entered name is already in list, please try again")
+               
+#                 elif userInput == "":
+#                     print("Please enter a players name, please try again")
+
+#                 else:
+#                     teamPlayerList[int(teamNumber)-1].append(userInput)
+#                     break
+        
+#         print(f"Members of team {teamNumber} are: {teamPlayerList[int(teamNumber)-1]} \n\n\n")
+#     break
+
+
+counter = 0
+
 while True:
 
-    # Loops for each team
-    for teamNumber in range(1,5):
-        print(f"ADDING PLAYERS TO TEAM {teamNumber}")
-        
-        # Loops for each player in team
-        for playerNumber in range(1,6):
+    if counter == 20:
+        print("Max number of players added... moving on")
+        break
 
-            # loops untill valid player is added to team
-            while True:
+    userInputplayer = input(f"\n--\nEnter the name of player No.{counter + 1} >>> ")
 
-                userInput = input(f"Enter the name for player {playerNumber} >>> ")
+    # checks if stop command is given and if 5 or more players are added, if yes, break
+    if userInputplayer == "stop" or userInputplayer == "-":
+        if counter < 5:
+            print("At least 5 players must be added before moving on... Please try again\n")
+            print(f"you have entered {counter} Players, you need at least 5 to move on\n\n")
+
+        else:
+            print("stop commaned detected, moving on...")
+            break
+
+    # Same validation as the last section
+    elif userInputplayer in playerScoreDict:
+
+        print(f"A player called {userInputplayer} already exists, please try again")
+
+    elif userInputplayer == "":
+        print("Please enter a players name, please try again")
+
+    else:
+        playerScoreDict[userInputplayer] = 0
+
+
+
+        # user selects which events the newly added player will take part in
+        while True:
+            userInputEvent = input("\n1-5 = single event, '' = all solo events >>> ")
+            
+            # if user selected all events, add player to all event lists and print result
+            if userInputEvent == "":
+                for i in range(len(eventPlayersList)):
+                    eventPlayersList[i].append(userInputplayer)
+
+                break
+            
+            #else if user entered value between 1 and 5, add player to event list at index
+            elif int(userInputEvent) >0 and int(userInputEvent) <6:
                 
-                # validates input:
-                # NOTE Add more and propper validation later
+                eventPlayersList[int(userInputEvent)-1].append(userInputplayer)
+                break
 
-                if userInput in teamPlayerList[int(teamNumber)-1]:
-                    print("entered name is already in list, please try again")
-               
-                elif userInput == "":
-                    print("Please enter a players name, please try again")
+            
+            else:
+                print("INVALID INPUT")
 
-                else:
-                    teamPlayerList[int(teamNumber)-1].append(userInput)
-                    break
-        
-        print(f"Members of team {teamNumber} are: {teamPlayerList[int(teamNumber)-1]} \n\n\n")
-    break
-
-
-
+        counter += 1
 
