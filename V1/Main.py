42e0e945e53f071c9ba94908a91310cdf4c8287b
Tom Horsman <thomashorsman99@gmail.com>
2025-04-17 15:51:05 +0100

    add basic create team and asign players

--- V1/Main.py
+++ V1/Main.py
@@ -1,7 +1,39 @@
 
 teamScoreDict = {}
+teamPlayerList = [[],[],[],[]]
 playerScoreDict = {}
 
 print("TEAM INTRO")
+
 while True:
-    pass
+
+    # Loops for each team
+    for teamNumber in range(1,5):
+        print(f"ADDING PLAYERS TO TEAM {teamNumber}")
+        
+        # Loops for each player in team
+        for playerNumber in range(1,6):
+
+            # loops untill valid player is added to team
+            while True:
+
+
+                userInput = input(f"Enter the name for player {playerNumber} >>> ")
+                
+                # validates input:
+                # NOTE Add more and propper validation later
+
+                if userInput in teamPlayerList[int(teamNumber)-1]:
+                    print("entered name is already in list, please try again")
+               
+                elif userInput == "":
+                    print("Please enter a players name, please try again")
+
+                else:
+                    teamPlayerList[int(teamNumber)-1].append(userInput)
+                    break
+                
+        print(teamPlayerList)
+    break
+
+
