5863c6b3ca12ec76684816e1da13dec793fecf6d
Tom Horsman <thomashorsman99@gmail.com>
2025-04-18 14:28:16 +0100

    add flow for event selection

--- V1/Main.py
+++ V1/Main.py
@@ -7,11 +7,6 @@ teamPlayerList = [[],[],[],[]]
 # tracks the scores for each solo player
 playerScoreDict = {}
 
-# holds the academic and sporty TEAM events
-teamEventList = [["Pub Quiz", "Treasure hunt"],["Football", "Rounders", "Relay"]]
-
-# holds the academic and sporty SOLO events
-soloEventList = [["Spelling bee","Treasure hunt"],["Sack race", "Running", "Egg and Spoon race"]]
 
 # tracks the SOLO players taking part in each event
 eventPlayersList=[[],[],[],[],[]]
@@ -21,11 +16,11 @@ teamPoints = [15,10,8,4]
 soloPoints = [15, 12,10, 8, 6, 3]
 
 
-print(teamEventList)
-print(soloEventList)
 
 print("TEAM INTRO")
+
 # NOTE COMMENT CODE BACK IN LATER
+
 # # whle loop for adding each team and asigning their players
 # while True:
 
@@ -58,60 +53,126 @@ print("TEAM INTRO")
 #     break
 
 
-counter = 0
-# user enters players for solo events
-while True:
 
-    if counter == 20:
-        print("Max number of players added... moving on")
+# NOTE add player section starts here
+
+# counter = 0
+# # user enters players for solo events
+# while True:
+
+#     if counter == 20:
+#         print("Max number of players added... moving on")
+#         break
+
+#     userInputplayer = input(f"\n--\nEnter the name of player No.{counter + 1} >>> ")
+
+#     # checks if stop command is given and if 5 or more players are added, if yes, break
+#     if userInputplayer == "stop" or userInputplayer == "-":
+#         if counter < 5:
+#             print("At least 5 players must be added before moving on... Please try again\n")
+#             print(f"you have entered {counter} Players, you need at least 5 to move on\n\n")
+
+#         else:
+#             print("stop commaned detected, moving on...")
+#             break
+
+#     # Same validation as the last section
+#     elif userInputplayer in playerScoreDict:
+
+#         print(f"A player called {userInputplayer} already exists, please try again")
+
+#     elif userInputplayer == "":
+#         print("Please enter a players name, please try again")
+
+#     else:
+#         playerScoreDict[userInputplayer] = 0
+
+
+
+#         # user selects which events the newly added player will take part in
+#         while True:
+#             userInputEvent = input("\n1-5 = single event, '' = all solo events >>> ")
+            
+#             # if user selected all events, add player to all event lists and print result
+#             if userInputEvent == "":
+#                 for i in range(len(eventPlayersList)):
+#                     eventPlayersList[i].append(userInputplayer)
+
+#                 break
+            
+#             #else if user entered value between 1 and 5, add player to event list at index
+#             elif int(userInputEvent) >0 and int(userInputEvent) <6:
+                
+#                 eventPlayersList[int(userInputEvent)-1].append(userInputplayer)
+#                 break
+
+            
+#             else:
+#                 print("INVALID INPUT")
+
+#         counter += 1
+
+# holds the academic and sporty events, first list is solo, second is team
+EventsList = [
+                 [[],[]], # Solo events
+                 [[],[]]  # Team events
+                 ]
+
+numberOfEventsInList = len(EventsList[0][0] + EventsList[0][1] + EventsList[1][0] + EventsList[1][1])
+
+
+    
+while True:
+    
+    if numberOfEventsInList == 0:
+        print("all events ran, bracking loop")
         break
 
-    userInputplayer = input(f"\n--\nEnter the name of player No.{counter + 1} >>> ")
+    while True:
 
-    # checks if stop command is given and if 5 or more players are added, if yes, break
-    if userInputplayer == "stop" or userInputplayer == "-":
-        if counter < 5:
-            print("At least 5 players must be added before moving on... Please try again\n")
-            print(f"you have entered {counter} Players, you need at least 5 to move on\n\n")
 
-        else:
-            print("stop commaned detected, moving on...")
+        userInput = int(input("1 = Solo\n2 = Team\n>>> "))
+
+        # if there are accedemic events left for the selected group, show those events
+        if EventsList[userInput-1][0] != []:
+            print("showing all avliable acedemic items in list\n")
+            print(EventsList[userInput-1][0])
+            output = "A"
             break
 
-    # Same validation as the last section
-    elif userInputplayer in playerScoreDict:
+        else:
 
-        print(f"A player called {userInputplayer} already exists, please try again")
-
-    elif userInputplayer == "":
-        print("Please enter a players name, please try again")
-
-    else:
-        playerScoreDict[userInputplayer] = 0
-
-
-
-        # user selects which events the newly added player will take part in
-        while True:
-            userInputEvent = input("\n1-5 = single event, '' = all solo events >>> ")
-            
-            # if user selected all events, add player to all event lists and print result
-            if userInputEvent == "":
-                for i in range(len(eventPlayersList)):
-                    eventPlayersList[i].append(userInputplayer)
-
-                break
-            
-            #else if user entered value between 1 and 5, add player to event list at index
-            elif int(userInputEvent) >0 and int(userInputEvent) <6:
+            # if selected group's accedemic events are empty, check if the other groups events are empty as well, 
+            # if yes, show the sporting events for the selected group
+            if EventsList[userInput % 2][0] == []:
                 
-                eventPlayersList[int(userInputEvent)-1].append(userInputplayer)
+                
+                # if that is also empty, 
+                if EventsList[userInput-1][1] == []:
+                    print("This group have performed all their events. But, there are still events in the other group")
+                    print(EventsList[userInput % 2][1])
+                    output = "B"
+                    break
+        
+                else:
+                    print("Showing sporing events for selected group\n")
+                    print(EventsList[userInput-1][1])
+                    output = "C"
+                    break
+        
+            # otherwise, show the accedemic events for the other group
+            else:    
+                print("acmd empty: here are the remainging acmd events in the other group\n")
+                print(EventsList[userInput % 2][0])
+                output = "D"
                 break
+        
+
+
+
+    print(output)
+    
 
             
-            else:
-                print("INVALID INPUT")
 
-        counter += 1
-
-
+print("this is the end")
